3400eeb8eb1add44ebdc3368d6b01e3e8ad1947c
chathush-vish <chathushuni@gmail.com>
2025-02-22 14:49:35 +0530

    adding form  page working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../components/LoginForm";
 
 function Home() {
+   const [isClickadd, setIsClickadd] = useState(false);
+
    return (
       <div className="w-full flex flex-col gap-5 items-center justify-center mt-10">
          <h3>MERN crud practice</h3>
-         <LoginForm />
+         <button
+            onClick={() => setIsClickadd(true)}
+            className="py-2 px-6 bg-gray-900 text-white rounded-lg cursor-pointer hover:bg-gray-800"
+         >
+            Add new user
+         </button>
+         {isClickadd && <LoginForm />}
       </div>
    );
 }
